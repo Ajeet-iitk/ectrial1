f5cdc29a08c83b6f5e4a423a0a2b5247c0283957
Ajeet Kushwaha <85423653+Ajeet-iitk@users.noreply.github.com>
2021-11-01 20:23:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,12 +21,16 @@ var fordiv = document.createElement('div');
 fordiv.classList.add('row' , 'portfolio-container');
 fordiv.style.position = "relative";
 fordiv.style.height = "1440px";
+fordiv.style.display = "flex";
+//fordiv.style.margin-right = "-15px";
+//fordiv.style.margin-left = "-15px";
 
 
 var k;
 const name1 = ["Ajeet K." , "Vedaant Coin" , "Anshika Sports"];
 const posts1 = ["President, Students’ Gymkhana" , "PRESIDENT, STUDENTS’ GYMKHANA" , "GENERAL SECRETARY, GAMES AND SPORTS"];
 const link1 = ["Aj-img.jpg" , "Vedaant.jpg" , "Anshika.jpg"];
+const html_link = ["https://eciitk.com/candidates/kartikgupta.html", "https://eciitk.com/candidates/vedaantsikka.html", "https://eciitk.com/candidates/anshikachaudhary.html"];
 for(k = 0; k < 3; k++)
 {
 	var div11 = document.createElement('div');
@@ -51,12 +55,22 @@ for(k = 0; k < 3; k++)
 
 	var div111 = document.createElement('div');
 	div111.classList.add("portfolio-info");
-	var a1 = document.createElement('h2');
-	var a1t = document.createTextNode(name1[k]);
-	a1.appendChild(a1t);
+	div111.style.height = "100px";
+	var a1 = document.createElement('h4');
+	a1.style.lineHeight = "1px";
+	a1.style.marginBottom = "18px";
+	var a11 = document.createElement('a');
+	a11.href = html_link[k];
+	a11.target = "_blank";
+	a11.innerHTML = name1[k];
+	a11.className = "highlight";
+	a11.classList.add('HoverClass1' , 'acolor');
+	//var a1t = document.createTextNode(name1[k]);
+	//a11.appendChild(a1t);
+	a1.appendChild(a11);
 	div111.appendChild(a1);
 
-	var p1 = document.createElement('h4');
+	var p1 = document.createElement('p');
 	var p1t = document.createTextNode(posts1[k]);
 	p1.appendChild(p1t);
 	div111.appendChild(p1);
@@ -65,9 +79,10 @@ for(k = 0; k < 3; k++)
 	div11.appendChild(div111);
 
 
-	div1.appendChild(div11);
+	
+	fordiv.appendChild(div11);
 }
-fordiv.appendChild(div11);
+
 
 div1.appendChild(fordiv);
 
@@ -107,44 +122,7 @@ document.getElementsByTagName("body")[0].appendChild(section1);
 
 
 
-var i;
-
-const name = ["Ajeet","Big Bro","Sis"];
-for(i=0; i < 3 ; i++)
-{
 
 
 
-
-var card1 = document.createElement("div");
-card1.id = "see";
-card1.classList.add('w3-card-4');
-
-var header1 = document.createElement('header');
-header1.classList.add('w3-container' , 'w3-light-grey');
-var h3text = document.createElement('h3');
-var h3text1 = document.createTextNode("hola");
-h3text.appendChild(h3text1);
-header1.appendChild(h3text);
-card1.appendChild(header1);
-
-var card2 = document.createElement('div');
-card2.classList.add('w3-container');
-var img1 = document.createElement('img');
-img1.src = "Aj-img.jpg";
-img1.style.width = "200px";
-img1.style.height = "200px";
-card2.appendChild(img1);
-card1.appendChild(card2);
-
-var h1text = document.createElement('P');
-var text1 = document.createTextNode("Name : " +name[i]);
-h1text.appendChild(text1);
-card1.appendChild(h1text);
-//document.getElementById('see').setAttribute("class", w3-cards-4);
-//document.getElementsByTagName("body")[0].appendChild(card1);
-
-
-//document.getElementsByTagName("body")[0].appendChild(card1);
-}
 
